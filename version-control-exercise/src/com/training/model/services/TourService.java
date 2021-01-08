f63a7a597844e3a1b4f472366793458373deb9c5
sangmesh <sapatil917@gmail.com>
2021-01-08 12:55:39 +0530

    REQ-B201 ADDED STATUS

--- version-control-exercise/src/com/training/model/services/TourService.java
+++ version-control-exercise/src/com/training/model/services/TourService.java
@@ -5,7 +5,13 @@ import com.training.model.Tour;
 public class TourService {
 
 	public int addTour(Tour tour) {
-		return 0;
+		int status=0;
+		if(tour!=null)
+		{
+			status=1;
+			
+		}
+		return status;
 		
 	}
 }
