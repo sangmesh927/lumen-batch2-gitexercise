722f141477e29f646e3006a3b4f5859781d718b4
sangmesh <sapatil917@gmail.com>
2021-01-08 12:37:44 +0530

    REQ-101 -Create Tour Class Completed

---
+++ version-control-exercise/src/com/training/model/Tour.java
@@ -0,0 +1,50 @@
+package com.training.model;
+
+public class Tour {
+
+	private int tourId;
+	private String tourName;
+	private double duration;
+	private double cost;
+	public Tour(int tourId, String tourName, double duration, double cost) {
+		super();
+		this.tourId = tourId;
+		this.tourName = tourName;
+		this.duration = duration;
+		this.cost = cost;
+	}
+	public Tour() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Tour [tourId=" + tourId + ", tourName=" + tourName + ", duration=" + duration + ", cost=" + cost + "]";
+	}
+	public int getTourId() {
+		return tourId;
+	}
+	public void setTourId(int tourId) {
+		this.tourId = tourId;
+	}
+	public String getTourName() {
+		return tourName;
+	}
+	public void setTourName(String tourName) {
+		this.tourName = tourName;
+	}
+	public double getDuration() {
+		return duration;
+	}
+	public void setDuration(double duration) {
+		this.duration = duration;
+	}
+	public double getCost() {
+		return cost;
+	}
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+	
+
+}
